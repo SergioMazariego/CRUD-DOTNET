82b4c3c0c6fc789cd9eec784bc81ee8d3ea6292b
Sergio Mazariego <sergioacuos@gmail.com>
2021-02-09 17:17:23 -0600

    Gender display convention

---
+++ Udemy/Controllers/AlumnoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Udemy.Models;
+
+namespace Udemy.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+
+            AlumnosContect db = new AlumnosContect();
+
+            return View(db.Alumno.ToList());
+        }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Alumno a)
+        {
+            return View();
+        }
+    }
+}
